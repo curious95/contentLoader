707180296cbcda179df715128057cfb08f1c094a
curious95 <kamalpradhan95@outlook.com>
2018-11-26 17:10:55 +0000

    test code v2

--- scripts/__init__py.py
+++ scripts/__init__py.py
@@ -1,18 +1,6 @@
-from browsermobproxy import Server
+from subprocess import check_output
+import json
 
-server = Server("path/to/browsermob-proxy")
-server.start()
-proxy = server.create_proxy()
-
-from selenium import webdriver
-profile  = webdriver.FirefoxProfile()
-profile.set_proxy(proxy.selenium_proxy())
-driver = webdriver.Firefox(firefox_profile=profile)
-
-
-proxy.new_har("google")
-driver.get("http://www.google.co.uk")
-proxy.har # returns a HAR JSON blob
-
-server.stop()
-driver.quit()
+out = check_output(['./phantomjs', '--ssl-protocol=any', \
+    '--web-security=false', 'getResources.js', 'www.google.com'])
+data = json.loads(out)
