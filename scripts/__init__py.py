dc486c39441487696ae879af6d5059bb905cfb6e
curious95 <kamalpradhan95@outlook.com>
2018-11-26 17:16:00 +0000

    Added phantom Js driver

--- scripts/__init__py.py
+++ scripts/__init__py.py
@@ -1,6 +1,6 @@
 from subprocess import check_output
 import json
 
-out = check_output(['./phantomjs', '--ssl-protocol=any', \
+out = check_output(['../phantomjs/phantomjs', '--ssl-protocol=any', \
     '--web-security=false', 'getResources.js', 'www.google.com'])
 data = json.loads(out)
