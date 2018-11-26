31ee843c882fd48a91ac146d8c985c61f85c292a
curious95 <kamalpradhan95@outlook.com>
2018-11-26 20:26:24 +0000

    Extracted Urls, Js, Css

--- scripts/__init__py.py
+++ scripts/__init__py.py
@@ -1,6 +1,16 @@
 from subprocess import check_output
+import re
+
 
 out = check_output(['../phantomjs/phantomjs', '--ssl-protocol=any', \
-    '--web-security=false', 'sniff.js', 'https://stackoverflow.com'])
+    '--web-security=false', 'sniff.js', 'https://goodsports.com/'])
 
-print(out)
+out = out.decode("utf-8")
+
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', out)
+# print("Original string: ",text)
+
+for url in urls:
+    print(url)
+
+#print("Urls: ",urls)
